5ba98d8ee93bbe43fa4813d25a820a9b3e55417c
Alisha Byford <serenity4eternity786@gmail.com>
2017-02-09 02:45:26 +0000

    installing dependencies

---
+++ build/react-capstone.1.0.0.js
@@ -0,0 +1,62 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	// This section would be where requirements happen.
+	// require("babel-core").transform("code", options);
+	
+	// This section would be where everything is defined.
+	
+	
+	// I will need to map out the variables and make sure that all the calls are made 
+	// the way that they are supposed to be made.
+	
+	
+	// This section will be the wrap up, any last additions will be made here.
+	"use strict";
+
+/***/ }
+/******/ ]);
+//# sourceMappingURL=react-capstone.1.0.0.js.map
---
+++ js/index.js
@@ -0,0 +1,12 @@
+// This section would be where requirements happen.
+// require("babel-core").transform("code", options);
+
+// This section would be where everything is defined.
+
+
+// I will need to map out the variables and make sure that all the calls are made 
+// the way that they are supposed to be made.
+
+
+
+// This section will be the wrap up, any last additions will be made here.
